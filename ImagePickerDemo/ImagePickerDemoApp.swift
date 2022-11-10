2781848ecfb6b3b38de80d3b76feac078a402748
Melvin W <melvinhw@me.com>
2022-11-10 10:20:33 +0000

    Initial Commit

---
+++ ImagePickerDemo/ImagePickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickerDemoApp.swift
+//  ImagePickerDemo
+//
+//  Created by M W on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
